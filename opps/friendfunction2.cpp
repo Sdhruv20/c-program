e83f76f8d694f25bfadf4e67dc211880a8ac596b
Sdhruv20 <dhruvsardhara20@gmail.com>
2024-09-08 14:04:00 +0530

    how to use entire friend function

---
+++ opps/friendfunction2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// forward declaration
+class complex;
+
+class calculator
+{
+public:
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+    int sumcomplex(complex, complex);
+    int sumcompcomplex(complex, complex);
+};
+
+class complex
+{
+    int a, b;
+
+    
+    friend class calculator;
+
+public:
+    void setnumber(int n1, int n2)
+    {
+
+        a = n1;
+        b = n2;
+    }
+    void printnumber()
+    {
+        cout << "your number is " << a << " + " << b << "i" << endl;
+    }
+};
+
+int calculator ::sumcomplex(complex o1, complex o2)
+{
+    return (o1.a + o2.a);
+}
+int calculator ::sumcompcomplex(complex o1, complex o2)
+{
+    return (o1.b + o2.b);
+}
+int main()
+{
+    complex o1, o2;
+    o1.setnumber(1, 4);
+    o2.setnumber(5, 8);
+    calculator calc;
+    int res = calc.sumcomplex(o1, o2);
+    cout << "the result of sum is " << res << endl;
+    int resc = calc.sumcompcomplex(o1, o2);
+    cout << "the result of sum is " << resc << endl;
+
+    return 0;
+}
---
+++ opps/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+ // friend int calculator ::sumcomplex(complex o1, complex o2);
+    // friend int calculator ::sumcompcomplex(complex o1, complex o2);
