e9046c06643d9b32dd83c91e9b40a34ddd825d97
Sdhruv20 <dhruvsardhara20@gmail.com>
2024-09-12 13:41:20 +0530

    constructors

--- opps/complexfunction.cpp
+++ opps/complexfunction.cpp
@@ -1,50 +1,63 @@
-#include<iostream>
+
+// using call by function through friend function
+
+#include <iostream>
 using namespace std;
 class c2;
 
-class c1{
+class c1
+{
     int val;
-    friend void sawp(c1 &,c2 &);
-    public:
-        void intdata(int a){
-                val=a;
-        }
+    friend void sawp(c1 &, c2 &);
 
-        void display(void){
-            cout<<val<<endl;
-        }
+public:
+    void intdata(int a)
+    {
+        val = a;
+    }
+
+    void display(void)
+    {
+        cout << val << endl;
+    }
 };
 
-class c2{
+class c2
+{
     int val2;
-      friend void sawp(c1 &,c2 &);
-    public:
-        void intdata(int b){
-                val2=b;
-        }
+    friend void sawp(c1 &, c2 &);
 
-        void display(void){
-            cout<<val2<<endl;
-        }
+public:
+    void intdata(int b)
+    {
+        val2 = b;
+    }
+
+    void display(void)
+    {
+        cout << val2 << endl;
+    }
 };
 
-void sawp(c1 & x, c2 & y){
-    int tmp =x.val;
-    x.val= y.val2;
-    y.val2=tmp;
+void sawp(c1 &x, c2 &y)
+{
+    int tmp = x.val;
+    x.val = y.val2;
+    y.val2 = tmp;
 }
 
-int main(){
+int main()
+{
     c1 oc1;
     c2 oc2;
 
     oc1.intdata(34);
     oc2.intdata(55);
-    sawp(oc1,oc2);
+    sawp(oc1, oc2);
 
-    cout<<"the value of c1 after exchanging become : ";
+    cout << "the value of c1 after exchanging become : ";
     oc1.display();
-     cout<<"the value of c2 after exchanging become : ";
+    cout << "the value of c2 after exchanging become : ";
     oc2.display();
 
     return 0;
---
+++ opps/constructors.cpp
@@ -0,0 +1,33 @@
+
+// constructors = a special member fuction with same name as of the class.
+// use to initializ the objects of its class
+// it is automatically invoked(call by something) whenever an objects is created
+
+#include<iostream>
+using namespace std;
+class complex{
+
+    int a, b;
+    public:
+        
+    complex(void); // constructor declaration
+    void display(void){
+        cout<<"your number is "<< a << " + "<<b<<endl;
+    }
+
+};
+
+// the scope resolation operator in C++ is [ "::" ]
+
+complex ::complex(void){
+    a=10;
+    b=5;
+
+};
+int main(){
+
+    complex c;
+    c.display();
+    
+    return 0;
+}
